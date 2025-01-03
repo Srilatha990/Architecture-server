243fc79731854144b6eb242ff99ba4bd3262cb02
Srilatha990 <deshisrilatha745@gmail.com>
2025-01-03 11:54:59 +0530

    first commit

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,17 +1,37 @@
+// // routes/serviceRoutes.js
+// const express = require('express');
+// const multer = require('multer');
+// const serviceController = require('../controllers/serviceController');
+
+// // Setup multer for handling image upload
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, './uploads'); // You can choose a different directory for your uploaded files
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + '-' + file.originalname);
+//   },
+// });
+// const upload = multer({ storage: storage });
+
+// const router = express.Router();
+
+// router.post('/services/insert', upload.single('image'), serviceController.addService);
+// router.get('/services', serviceController.getAllServices);
+// router.get('/services/:id', serviceController.getServiceById);
+// router.put('/services/:id', upload.single('image'), serviceController.updateService);
+// router.delete('/services/:id', serviceController.deleteService);
+
+// module.exports = router;
+
+
 // routes/serviceRoutes.js
 const express = require('express');
 const multer = require('multer');
 const serviceController = require('../controllers/serviceController');
 
-// Setup multer for handling image upload
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './uploads'); // You can choose a different directory for your uploaded files
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
+// Multer configuration
+const storage = multer.memoryStorage();  // Store files in memory instead of disk
 const upload = multer({ storage: storage });
 
 const router = express.Router();
